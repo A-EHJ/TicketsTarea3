3169b99d4febd4b7fc6e36625cac6c7d2ff43619
A-EHJ <abrahamelhage2003@gmail.com>
2023-09-18 13:21:51 -0400

    Creacion del contexto

---
+++ Tickets/DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Tickets.DAL
+{
+    public class Contexto: DbContext
+    {
+        //No me estaba permitiendo hacer using Tickets.Models; y despues nombrar a la clase Ticket, tuve que hacerlo de la forma que se ve abajo
+        public DbSet<Models.Tickets> Tickets { get; set; }
+        public Contexto(DbContextOptions<Contexto> options) : base(options) {}
+    }
+}
