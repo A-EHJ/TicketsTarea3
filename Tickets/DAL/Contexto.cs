b0e7cc1772e5e1bad7eb29e7cbf9e31c4358ad32
A-EHJ <abrahamelhage2003@gmail.com>
2023-09-20 13:58:39 -0400

    Quitando espacios

--- Tickets/DAL/Contexto.cs
+++ Tickets/DAL/Contexto.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 namespace Tickets.DAL
 {
-    public class Contexto: DbContext
+    public class Contexto : DbContext
     {
         //No me estaba permitiendo hacer using Tickets.Models; y despues nombrar a la clase Ticket, tuve que hacerlo de la forma que se ve abajo
         public DbSet<Models.Tickets> Tickets { get; set; }
-        public Contexto(DbContextOptions<Contexto> options) : base(options) {}
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
     }
 }
